2e10ab866c7c491445e7cdab6a4b53b2ae79ef96
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2023-03-23 13:29:28 -0300

    refactor: Pokemons - model choices and post view

--- pokemons/models.py
+++ pokemons/models.py
@@ -1,7 +1,34 @@
 from uuid import uuid4
 from django.db import models
 
-from utils.pokemons.choices import RARITY_CHOICES, TYPE_CHOICES
+TYPE_CHOICES = (
+    ("normal", "normal"),
+    ("fire", "fire"),
+    ("water", "water"),
+    ("grass", "grass"),
+    ("electric", "electric"),
+    ("ice", "ice"),
+    ("fighting", "fighting"),
+    ("poison", "poison"),
+    ("ground", "ground"),
+    ("flying", "flying"),
+    ("psychic", "psychic"),
+    ("bug", "bug"),
+    ("rock", "rock"),
+    ("ghost", "ghost"),
+    ("dark", "dark"),
+    ("dragon", "dragon"),
+    ("steel", "steel"),
+    ("fairy", "fairy"),
+)
+
+RARITY_CHOICES = (
+    ("C", "common"),
+    ("R", "rare"),
+    ("E", "epic"),
+    ("L", "legendary"),
+)
+
 
 class Pokemons(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
--- pokemons/views.py
+++ pokemons/views.py
@@ -1,6 +1,6 @@
 from django.forms import model_to_dict
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
-from rest_framework.views import Response, Request,status, APIView
+from rest_framework.views import Response, Request, status, APIView
 from pokemons.models import Pokemons
 
 from pokemons.serializers import PokemonSerializer
@@ -14,21 +14,17 @@ class PokemonListCreateView(ListCreateAPIView):
         is_many = isinstance(request.data, list)
 
         if is_many:
-            serializer = self.get_serializer(data=request.data, many=True)
+            serializer = PokemonSerializer(data=request.data, many=True)
             serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
-            headers = self.get_success_headers(serializer.data)
-
-            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         serializer = PokemonSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-
-        return Response(serializer.data, status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class PokemonDetailView(RetrieveAPIView):
-        serializer_class = PokemonSerializer
-        queryset = Pokemons.objects.all()
-
+    serializer_class = PokemonSerializer
+    queryset = Pokemons.objects.all()
