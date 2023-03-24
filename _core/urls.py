d5da00977efdc471e2c81a469bf815a63aaea3d1
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2023-03-24 18:38:09 -0300

    feat: User - model, CRUD and pokemon relation

--- _core/urls.py
+++ _core/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("pokemons/", include("pokemons.urls")),
+    path("users/", include("users.urls")),
 ]
