3990cb013927b39c2ba0be4baf7b05dc9bc41dad
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2023-03-27 16:36:34 -0300

    refactor: User - Hash password

--- users/models.py
+++ users/models.py
@@ -6,10 +6,6 @@ from django.contrib.auth.hashers import make_password
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    email = models.EmailField(unique=True)
     gold = models.IntegerField(default=100)
     dateRoll = models.IntegerField(default=0)
-
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            self.password = make_password(self.password)
-        super().save(*args, **kwargs)
